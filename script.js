0791fac5e2df9273f07e1d6a9f42501ed000e4cc
Matias Gonzalez <you@example.com>
2025-02-04 08:34:21 -0300

    agregue la funcion arrendar pelicula con el metodo splice

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-// > <
-
-// cartelera de cine 
-
 const peliculasTodas = ["Volver al futuro 1","Titanic","Rocky 1"];
 const peliculasStock = ["Volver al futuro 1","Titanic"];
 const peliculasArrendadas = ["Rocky 1"];
 let opcion;
+let pelicula;
 
 function todasPeliculas() {
-    if(peliculasTodas.length ===0){
-        alert("No tenmos peliculas");
+    if (peliculasTodas.length === 0) {
+        alert("No tenemos películas");
     } else {
-        alert("Peliculas en el catalogo");
+        alert("Películas en el catálogo:");
         alert(peliculasTodas.join("\n"));
     }
 }
 
 function stockPeliculas() {
-    if(peliculasStock.length ===0){
-        alert("No nos quedan peliculas, vuelve pronto, o no tan pronto...");
+    if (peliculasStock.length === 0) {
+        alert("No nos quedan películas, vuelve pronto, o no tan pronto...");
     } else {
-        alert("Peliculas disponibles: ");
+        alert("Películas disponibles:");
         alert(peliculasStock.join("\n"));
     }
 }
 
-do {
+function arrendarPelicula() {
+    if (peliculasStock.length === 0) {
+        alert("No nos quedan películas para arrendar, vuelve luego...o no vuelvas, somos pobres");
+    } else {
+        pelicula = prompt("Ingresa el nombre de la película que quisieras arrendar:");
+        let peliculaEncontrada = false;
+        for (let index = 0; index < peliculasStock.length; index++) {
+            if (pelicula === peliculasStock[index]) {
+                peliculasStock.splice(index, 1);
+                peliculasArrendadas.push(pelicula);
+                alert("Película arrendada con éxito...");
+                peliculaEncontrada = true;
+                break;
+            }
+        }
+        if (!peliculaEncontrada) {
+            alert("No la tenemos disponible, pregunta por otra");
+        }
+    }
+}
 
-    opcion = parseInt(prompt("Bienvenido al Videoclub Pobre \n Elige tu opcion en el menu: \n 1) Ver listado de todas las peliculas. \n 2)Ver el listado de peliculas dispoibles: \n 3)Arrendar una pelicula"));
+do {
+    opcion = parseInt(prompt("Bienvenido al Videoclub Pobre \n Elige tu opción en el menú: \n 1) Ver listado de todas las películas. \n 2) Ver el listado de películas disponibles: \n 3) Arrendar una película"));
 
     switch (opcion) {
         case 1:
@@ -37,7 +54,8 @@ do {
             stockPeliculas();
             break;
         case 3:
-            alert("Arrendar una pelicula")
+            arrendarPelicula();
             break;
-    };
-} while (opcion !==0);
+    }
+} while (opcion !== 0);
+
